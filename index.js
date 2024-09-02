8e6bb6fd943b461de6c75ead045c06f17c4396cd
Debayan mukherjee <120113934+uxRakhal@users.noreply.github.com>
2024-09-03 01:42:41 +0530

    ANIMS

--- index.js
+++ index.js
@@ -8,3 +8,28 @@ burger.addEventListener('change', function () {
         navLinks.classList.remove('active');
     }
 });
+document.getElementById('main').style.display = 'none';
+setTimeout(() => {
+    document.getElementById('loader').style.display = 'none';
+    document.getElementById('main').style.display = 'block';
+}, 3000);
+
+
+
+const animate = document.querySelectorAll('.animate');
+
+const observer1 = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('doAnimation');
+        } else {
+            entry.target.classList.remove('doAnimation');
+        }
+    });
+});
+
+
+animate.forEach(anim => {
+    observer1.observe(anim);
+});
+
